df9425ceff7b2d7bbc7449de74be330b6b3de3f1
annferny <63791333+annferny@users.noreply.github.com>
2020-05-08 14:57:44 +0300

    Загрузка файлов сайта

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Страничка</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <?= $header ?>
+</header>
---
+++ nastya.php
@@ -0,0 +1,19 @@
+<?php
+$header = '<h1>Предметы ручной работы</h1>';
+$content = '<h2>Галерея</h2><img src="bag.jpg">';
+$aside = '<ul >
+            <li>Главная</li>
+            <li>Галерея</li>
+            <li>Доставка</li>
+            <li>Контакты</li>
+          </ul>';
+$footer = '<p>Телефон: +7888888888</p>
+           <p>e-mail: email@mail.ru</p>';
+
+include __DIR__ . '/header.php';
+include __DIR__ . '/aside.php';
+include __DIR__ . '/main.php';
+include __DIR__ . '/footer.php';
+
+
+
